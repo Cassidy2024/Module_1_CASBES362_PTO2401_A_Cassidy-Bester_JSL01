14189c86b7c8b90fa4dd6e1e9d688391edf30273
Cassidy2024 <157302466+Cassidy2024@users.noreply.github.com>
2024-05-17 20:26:50 +0400

    validation solution

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
+   document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('validateButton').addEventListener('click', function() {
+    //Event listener added to validate button to validate text input from the user
+        let input = document.getElementById('petInput').value;
+   
+    let pattern= /^pet_\d{4}\w+/; //defined pattern that the user must follow in order for input to be valid
 
-            document.getElementById('result').innerText = result;
-}
+    let result;
+    
+    if (pattern.test(input)) {  //checks that if input follows the pattern it will return "valid syntax" or "Invalid Syntax" if not
+        
+        result = "Valid Syntax" + '\u{1F7E2}' ;
+    } else {
+        result = "Invalid Syntax" + '\u{1F534}';
+        
+        }
+   
+
+    
+     
+    
+            document.getElementById('result').innerText = result; //the result will be shown on the webpage since result is the ID of a <p>
+        });
+ })();
+
 
 
